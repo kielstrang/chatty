8f6b73e48300ee5e5db30f934b80a8a69cec73c9
vagrant <kiel.strang@gmail.com>
2017-10-03 21:00:07 +0000

    Convert message input to controlled component

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,18 +4,24 @@ import PropTypes from 'proptypes';
 class ChatBar extends Component {
   constructor(props) {
     super(props);
-    this.onEnterMessage = this.onEnterMessage.bind(this);
+    this.state = { content: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   static propTypes = {
     currentUser: PropTypes.object,
     addMessage: PropTypes.func
   }
 
-  onEnterMessage(event) {
+  handleChange(event) {
+    this.setState({ content: event.target.value });
+  }
+
+  handleSubmit(event) {
     if(event.key === 'Enter') {
-      const message = {content: event.target.value, username: this.props.currentUser.name};
+      const message = { content: event.target.value, username: this.props.currentUser.name };
       this.props.addMessage(message);
-      event.target.value = '';
+      this.setState({ content: '' });
     }
   }
 
@@ -23,7 +29,12 @@ class ChatBar extends Component {
     return (
       <footer className="chatbar">
         <input className="chatbar-username" defaultValue={this.props.currentUser.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.onEnterMessage}/>
+        <input
+          className="chatbar-message"
+          placeholder="Type a message and hit ENTER"
+          value={this.state.content}
+          onChange={this.handleChange}
+          onKeyPress={this.handleSubmit} />
       </footer>
     );
   }
