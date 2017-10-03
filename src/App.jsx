5ce34ffa6f3428457423dd4f914b04656c0a7c19
vagrant <kiel.strang@gmail.com>
2017-10-03 23:57:10 +0000

    Send messages to server

--- src/App.jsx
+++ src/App.jsx
@@ -26,13 +26,22 @@ class App extends Component {
   }
 
   addMessage = (message) => {
-    message.id = this.state.nextID;
-    const messages = this.state.messages.concat(message);
-    this.setState({ messages: messages, nextID: this.state.nextID + 1 });
+    // message.id = this.state.nextID;
+    // const messages = this.state.messages.concat(message);
+    // this.setState({ messages: messages, nextID: this.state.nextID + 1 });
+    console.log('Sending message', JSON.stringify(message));
+    this.socket.send(JSON.stringify(message));
   }
 
   changeUser = (name) => {
     this.setState({ currentUser: { name } });
+  }
+
+  componentDidMount() {
+    this.socket = new WebSocket('ws://localhost:3001');
+    this.socket.onopen = () => {
+      console.log('Connected to server');
+    };
   }
 
   render() {
