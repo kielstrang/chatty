6b0159b89a84f7e7d11b29f8846e0d0e86c5658c
vagrant <kiel.strang@gmail.com>
2017-10-03 20:49:45 +0000

    cleanup chatbar state

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,28 +4,18 @@ import PropTypes from 'proptypes';
 class ChatBar extends Component {
   constructor(props) {
     super(props);
-    this.onContent = this.onContent.bind(this);
-    this.onKeyPress = this.onKeyPress.bind(this);
+    this.onEnterMessage = this.onEnterMessage.bind(this);
   }
   static propTypes = {
     currentUser: PropTypes.object,
     addMessage: PropTypes.func
   }
 
-  onContent(event) {
-    this.setState({
-      content: event.target.value
-    });
-  }
-
-  onKeyPress(event) {
+  onEnterMessage(event) {
     if(event.key === 'Enter') {
-      const message = {content: this.state.content, username: this.props.currentUser.name};
+      const message = {content: event.target.value, username: this.props.currentUser.name};
       this.props.addMessage(message);
       event.target.value = '';
-      this.setState({
-        content: ''
-      });
     }
   }
 
@@ -33,7 +23,7 @@ class ChatBar extends Component {
     return (
       <footer className="chatbar">
         <input className="chatbar-username" defaultValue={this.props.currentUser.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onChange={this.onContent} onKeyPress={this.onKeyPress}/>
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={this.onEnterMessage}/>
       </footer>
     );
   }
