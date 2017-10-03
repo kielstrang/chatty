29e302edf6aff6e352f779607d9413ea78f4d548
vagrant <kiel.strang@gmail.com>
2017-10-03 18:22:57 +0000

    Display current user name

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,30 @@ import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: { name: 'Bob' }, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?',
+        },
+        {
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
+        }
+      ]
+    };
+  }
+
   render() {
     return (
       <div>
         <NavBar />
         <MessageList />
-        <ChatBar />
+        <ChatBar currentUser={this.state.currentUser} />
       </div>
     );
   }
